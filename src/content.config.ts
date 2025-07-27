c6ce17fc3772d188eedbfa6be034c7b8cd0d2173
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-07-27 14:38:02 +0200

    chore: Modify blog content object

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,9 +3,10 @@ import { file, glob } from 'astro/loaders';
 
 const blog = defineCollection({
   loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/blog" }),
-  schema: z.object({
+  schema: ({ image }) => z.object({
     title: z.string(),
     description: z.string(),
+    coverImg: image(),
     pubDate: z.coerce.date(),
     tags: z.array(z.string()).min(1).max(4),
   })
