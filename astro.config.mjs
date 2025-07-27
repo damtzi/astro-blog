d9da960ccfff803ad37fed6e918317ccda7c24f9
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-07-27 15:47:56 +0200

    chore: Add imageService

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    adapter: cloudflare(),
+    adapter: cloudflare({
+        imageService: 'compile'
+    }),
 
     vite: {
         plugins: [tailwindcss()]
